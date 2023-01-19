433d3ebeeeb267df7ade62170bda6da58a879ee5
Leandro Freire <fotografoleandro81@gmail.com>
2023-01-18 21:38:22 -0300

    Remove ignored files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDMiNNJ-X_2ero0qXlfM2nY6utIJ-DaU5M",
+  authDomain: "greydive-f252b.firebaseapp.com",
+  projectId: "greydive-f252b",
+  storageBucket: "greydive-f252b.appspot.com",
+  messagingSenderId: "559917735984",
+  appId: "1:559917735984:web:a67976b235820dd4b98df5",
+  measurementId: "G-G9DHQ6CGMY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
